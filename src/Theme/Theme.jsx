39d790e4e3356471593c1952b76595c5c7306a77
thegittyuser <96658204+thegittyuser@users.noreply.github.com>
2025-08-19 19:57:38 +0500

    Theme Context To Be Continue

---
+++ src/Theme/Theme.jsx
@@ -0,0 +1,14 @@
+import "./theme.css";
+
+function Theme() {
+  return (
+    <>
+      <div className="theme-box-dark">
+        <h3>ThemeContext </h3>
+        <button>Toggle Theme</button>
+      </div>
+    </>
+  );
+}
+
+export default Theme;
