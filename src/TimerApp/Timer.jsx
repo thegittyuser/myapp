d30404e6a285737ad519a9885b298c5cb7f742d9
thegittyuser <96658204+thegittyuser@users.noreply.github.com>
2025-08-22 20:01:32 +0500

    TImerApp

--- src/TimerApp/Timer.jsx
+++ src/TimerApp/Timer.jsx
@@ -1,8 +1,32 @@
+import { useRef, useState } from "react";
 import "./timer.css";
 function Timer() {
+  const [seconds, setSeconds] = useState(0);
+  const timerRef = useRef(null);
+  const startTimer = () => {};
+
+  const stopTimer = () => {};
+
+  const resetTimer = () => {};
   return (
     <>
-      <h1>Timer App - React JS</h1>
+      <div className="main-box">
+        <h1>Timer App - React JS</h1>
+
+        <p className="timer start">Current Timer Value Is</p>
+        <p className="timer stop">Current Timer Stop Value Is</p>
+        <p className="timer reset">Timer Reset</p>
+
+        <button className="btn timer-start" onClick={startTimer}>
+          Start Timer
+        </button>
+        <button className="btn timer-stop" onClick={stopTimer}>
+          Stop Timer
+        </button>
+        <button className="btn timer-reset" onClick={resetTimer}>
+          Reset Timer
+        </button>
+      </div>
     </>
   );
 }
